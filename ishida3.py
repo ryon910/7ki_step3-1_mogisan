61f52904ba2c4a06a6bb340a14425c2c94f93ec1
ryon <ryo.nakano.eng@gmail.com>
2024-06-04 01:06:52 +0900

    no message

--- ishida3.py
+++ ishida3.py
@@ -17,7 +17,7 @@ load_dotenv()
 # 環境変数から認証情報を取得
 SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
 PRIVATE_KEY_PATH = os.getenv("PRIVATE_KEY_PATH")
-SP_SHEET = 'tech0_01'  # sheet名
+SP_SHEET     = 'tech0_01' # sheet名
 
 # セッション状態の初期化
 if 'show_all' not in st.session_state:
@@ -31,27 +31,27 @@ def toggle_show_all():
 def load_data_from_spreadsheet():
     # googleスプレッドシートの認証 jsonファイル読み込み(key値はGCPから取得)
     SP_CREDENTIAL_FILE = PRIVATE_KEY_PATH
-    
-    # ファイルの存在を確認
-    if not os.path.exists(SP_CREDENTIAL_FILE):
-        st.error(f"サービスアカウント認証情報ファイルが見つかりません: {SP_CREDENTIAL_FILE}")
-        return None
-    
+
     scopes = [
         'https://www.googleapis.com/auth/spreadsheets',
         'https://www.googleapis.com/auth/drive'
     ]
-    
-    try:
-        credentials = Credentials.from_service_account_file(SP_CREDENTIAL_FILE, scopes=scopes)
-    except Exception as e:
-        st.error(f"認証情報の読み込みに失敗しました: {e}")
-        return None
 
-    client = gspread.authorize(credentials)
-    sheet = client.open_by_key(SPREADSHEET_ID).worksheet(SP_SHEET)
-    data = sheet.get_all_records()
-    df = pd.DataFrame(data)
+    credentials = Credentials.from_service_account_file(
+        SP_CREDENTIAL_FILE,
+        scopes=scopes
+    )
+    gc = gspread.authorize(credentials)
+
+    SP_SHEET_KEY = SPREADSHEET_ID # d/〇〇/edit の〇〇部分
+    sh  = gc.open_by_key(SP_SHEET_KEY)
+
+    # 不動産データの取得
+    worksheet = sh.worksheet(SP_SHEET) # シートのデータ取得
+    pre_data  = worksheet.get_all_values()
+    col_name = pre_data[0][:]
+    df = pd.DataFrame(pre_data[1:], columns=col_name) # 一段目をカラム、以下データフレームで取得
+
     return df
 
 # データフレームの前処理を行う関数
@@ -68,61 +68,117 @@ def preprocess_dataframe(df):
 def make_clickable(url, name):
     return f'<a href="{url}" target="_blank">{name}</a>'
 
-def display_search_results(filtered_df):
-    # 結果を表示する
-    st.write("検索結果", filtered_df.to_html(escape=False, index=False), unsafe_allow_html=True)
+# 地図を作成し、マーカーを追加する関数
+def create_map(filtered_df):
+    # 地図の初期設定
+    map_center = [filtered_df['latitude'].mean(), filtered_df['longitude'].mean()]
+    m = folium.Map(location=map_center, zoom_start=12)
 
-def create_map(df):
-    # 地図を作成する
-    m = folium.Map(location=[df['latitude'].mean(), df['longitude'].mean()], zoom_start=13)
-    for idx, row in df.iterrows():
-        folium.Marker(
-            [row['latitude'], row['longitude']],
-            popup=folium.Popup(make_clickable(row['url'], row['物件名']), max_width=300),
-            tooltip=row['物件名']
-        ).add_to(m)
+    # マーカーを追加
+    for idx, row in filtered_df.iterrows():
+        if pd.notnull(row['latitude']) and pd.notnull(row['longitude']):
+            # ポップアップに表示するHTMLコンテンツを作成
+            popup_html = f"""
+            <b>名称:</b> {row['名称']}<br>
+            <b>アドレス:</b> {row['アドレス']}<br>
+            <b>家賃:</b> {row['家賃']}万円<br>
+            <b>間取り:</b> {row['間取り']}<br>
+            <b>面積:</b> {row['面積']}<br>
+            <b>最寄り駅:</b> {row['最寄り駅']}<br>
+            <a href="{row['物件詳細URL']}" target="_blank">物件詳細</a>
+            """
+            # HTMLをポップアップに設定
+            popup = folium.Popup(popup_html, max_width=400)
+            folium.Marker(
+                [row['latitude'], row['longitude']],
+                popup=popup
+            ).add_to(m)
+
     return m
 
+# 検索結果を表示する関数
+def display_search_results(filtered_df):
+    # 物件番号を含む新しい列を作成
+    filtered_df['物件番号'] = range(1, len(filtered_df) + 1)
+    filtered_df['物件詳細URL'] = filtered_df['物件詳細URL'].apply(lambda x: make_clickable(x, "リンク"))
+    display_columns = ['物件番号', '名称', 'アドレス', '階数', '家賃', '間取り', '面積','最寄り駅','物件詳細URL']
+    filtered_df_display = filtered_df[display_columns]
+    st.markdown(filtered_df_display.to_html(escape=False, index=False), unsafe_allow_html=True)
+
+# メインのアプリケーション
 def main():
     df = load_data_from_spreadsheet()
-    if df is None:
-        st.stop()
-    
     df = preprocess_dataframe(df)
-    
-    # 入力フォームの作成
-    st.title("不動産検索アプリ")
-    st.write("最寄り駅を選択してください")
-    
-    station_list = sorted(set(df['最寄り駅'].str.split(',').sum()))
-    selected_station = st.multiselect('駅名', station_list)
-    
-    filtered_df = df[df['最寄り駅'].str.split(',').apply(lambda x: any(station in x for station in selected_station))]
+
+    # StreamlitのUI要素（スライダー、ボタンなど）の各表示設定
+    st.title('賃貸物件情報の可視化')
+
+    # エリアと家賃フィルタバーを1:2の割合で分割
+    col1, col2 = st.columns([1, 2])
+
+    with col1:
+        # エリア選択
+        selected_areas = st.multiselect('■ エリア選択', df['区'].unique())
+
+    # 最寄り駅をエリア選択に基づいてフィルタリング
+    if selected_areas:
+        area_filtered_df = df[df['区'].isin(selected_areas)]
+        available_stations = area_filtered_df['最寄り駅'].str.split(',').explode().unique()
+    else:
+        available_stations = df['最寄り駅'].str.split(',').explode().unique()
+
+    with col2:
+        # 家賃範囲選択のスライダーをfloat型で設定し、小数点第一位まで表示
+        price_min, price_max = st.slider(
+            '■ 家賃範囲 (万円)', 
+            min_value=float(1), 
+            max_value=float(df['家賃'].max()),
+            value=(float(df['家賃'].min()), float(df['家賃'].max())),
+            step=0.1,  # ステップサイズを0.1に設定
+            format='%.1f'
+        )
+
+        # 最寄り駅選択
+        nearest_station = st.multiselect(
+            '■ 最寄り駅選択',
+            available_stations
+        )
+
+    with col2:
+        # 間取り選択のデフォルト値をすべてに設定
+        type_options = st.multiselect('■ 間取り選択', df['間取り'].unique(), default=df['間取り'].unique())
+
+    # フィルタリング/ フィルタリングされたデータフレームの件数を取得
+    filtered_df = df[df['区'].isin(selected_areas)]
+    filtered_df = filtered_df[filtered_df['間取り'].isin(type_options)]
+    filtered_df = filtered_df[(filtered_df['家賃'] >= price_min) & (filtered_df['家賃'] <= price_max)]
+    if nearest_station:
+        filtered_df = filtered_df[filtered_df['最寄り駅'].str.split(',').apply(lambda x: any(station in x for station in nearest_station))]
     filtered_count = len(filtered_df)
-    
+
     # 'latitude' と 'longitude' 列を数値型に変換し、NaN値を含む行を削除
     filtered_df['latitude'] = pd.to_numeric(filtered_df['latitude'], errors='coerce')
     filtered_df['longitude'] = pd.to_numeric(filtered_df['longitude'], errors='coerce')
     filtered_df2 = filtered_df.dropna(subset=['latitude', 'longitude'])
-    
-    # 検索ボタン / フィルタリングされたデータフレームの件数を表示
+
+    # 検索ボタン / # フィルタリングされたデータフレームの件数を表示
     col2_1, col2_2 = st.columns([1, 2])
-    
+
     with col2_2:
         st.write(f"物件検索数: {filtered_count}件 / 全{len(df)}件")
-    
+
     # 検索ボタン
     if col2_1.button('検索＆更新', key='search_button'):
         # 検索ボタンが押された場合、セッションステートに結果を保存
         st.session_state['filtered_df'] = filtered_df
         st.session_state['filtered_df2'] = filtered_df2
         st.session_state['search_clicked'] = True
-    
+
     # Streamlitに地図を表示
     if st.session_state.get('search_clicked', False):
         m = create_map(st.session_state.get('filtered_df2', filtered_df2))
         folium_static(m)
-    
+
     # 地図の下にラジオボタンを配置し、選択したオプションに応じて表示を切り替える
     show_all_option = st.radio(
         "表示オプションを選択してください:",
@@ -130,16 +186,17 @@ def main():
         index=0 if not st.session_state.get('show_all', False) else 1,
         key='show_all_option'
     )
-    
+
     # ラジオボタンの選択に応じてセッションステートを更新
     st.session_state['show_all'] = (show_all_option == 'すべての検索物件')
-    
+
     # 検索結果の表示
     if st.session_state.get('search_clicked', False):
         if st.session_state['show_all']:
             display_search_results(st.session_state.get('filtered_df', filtered_df))  # 全データ
         else:
             display_search_results(st.session_state.get('filtered_df2', filtered_df2))  # 地図上の物件のみ
+
 
 # アプリケーションの実行
 if __name__ == "__main__":
